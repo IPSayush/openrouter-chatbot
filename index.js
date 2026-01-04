55c47d6e65845eb723597637a9f200fe2a5e2fb9
ayush8627 <pnchalprakashan@gmail.com>
2026-01-04 21:47:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import OpenAI from "openai";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new OpenAI({
+  apiKey: process.env.OPENROUTER_API_KEY,
+  baseURL: "https://openrouter.ai/api/v1",
+});
+
+async function chat() {
+  const res = await client.chat.completions.create({
+model: "nvidia/nemotron-nano-9b-v2:free",
+    messages: [
+      { role: "user", content: "are you chat gpt?" }
+    ],
+  });
+
+  console.log("🤖 Bot:", res.choices[0].message.content);
+}
+
+chat();
